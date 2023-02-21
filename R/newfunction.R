e6c9b79fb651c8608e9d552a58bf300d909dbf6f
Kristen McGreevy <kmmmasterpiece@gmail.com>
2023-02-21 14:58:21 -0800

    test with Dr. Crespi

---
+++ R/newfunction.R
@@ -0,0 +1,17 @@
+#' This is test function
+#'
+#' This should be new description
+#'
+#' @param a First value to be added
+#' @param b Second value to be added
+#'
+#' @return The sum of a + b
+#' @export
+#'
+#' @examples
+#' # Try to show new function
+#' newfunction(a=3, b = 6)
+newfunction <- function(a, b){
+  c <- a + b
+  return(c)
+}
---
+++ tests/testthat/test-newfunction.R
@@ -0,0 +1,5 @@
+test_that("new function multiplies", {
+  # df <- data.frame(x=c(3,4), y = c(6,7))
+
+  expect_equal(newfunction(a = 3, b= 5), 8)
+})
