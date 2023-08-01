78b52442a03131794dabc54ae2206f9400157f8f
Zichen Liu <77468946+zichen-liu@users.noreply.github.com>
2023-07-31 17:42:13 -0700

    small edits to 1samp and paired

--- R/pss.t.test.1samp.R
+++ R/pss.t.test.1samp.R
@@ -2,7 +2,7 @@
 #'
 #' @param n The sample size.
 #' @param delta muA (the true mean) - mu0 (the mean under the null).
-#' @param sigma The estimated standard deviation; defaults to 1.
+#' @param sd The estimated standard deviation; defaults to 1.
 #' @param alpha The significance level or type 1 error rate; defaults to 0.05.
 #' @param power The specified level of power.
 #' @param one.or.two.sided Either "one" or "two" (default) to specify a one- or two- sided hypothesis test.
@@ -13,21 +13,21 @@
 #'
 #' @examples
 #' # Example 3.2
-#' pss.t.test.1samp(n = 36, delta = 4.9 - 5.7, sigma = 2, one.or.two.sided = "one")
+#' pss.t.test.1samp(n = 36, delta = 4.9 - 5.7, sd = 2, one.or.two.sided = "one")
 #' # Example 3.3
-#' pss.t.test.1samp(n = 36, delta = 6.3 - 5.7, sigma = 2, one.or.two.sided = "one")
+#' pss.t.test.1samp(n = 36, delta = 6.3 - 5.7, sd = 2, one.or.two.sided = "one")
 #' # Example 3.5
-#' pss.t.test.1samp(n = 36, delta = 4.9 - 5.7, sigma = 2, one.or.two.sided = "two")
+#' pss.t.test.1samp(n = 36, delta = 4.9 - 5.7, sd = 2, one.or.two.sided = "two")
 #' # Example 3.6
-#' pss.t.test.1samp(delta = 0.6, sigma = 1, power = 0.8, one.or.two.sided = "one")
+#' pss.t.test.1samp(delta = 0.6, sd = 1, power = 0.8, one.or.two.sided = "one")
 
-pss.t.test.1samp <- function (n = NULL, delta = NULL, sigma = 1,
+pss.t.test.1samp <- function (n = NULL, delta = NULL, sd = 1,
                               alpha = 0.05, power = NULL,
                               one.or.two.sided = c("two", "one"), strict = TRUE) {
 
   # Check if the arguments are specified correctly
-  if (sum(sapply(list(n, delta, sigma, power, alpha), is.null)) != 1)
-    stop("exactly one of n, delta, sigma, alpha, and power must be NULL")
+  if (sum(sapply(list(n, delta, sd, power, alpha), is.null)) != 1)
+    stop("exactly one of n, delta, sd, alpha, and power must be NULL")
 
   # Assign number of sides
   one.or.two.sided <- match.arg(one.or.two.sided)
@@ -40,13 +40,13 @@ pss.t.test.1samp <- function (n = NULL, delta = NULL, sigma = 1,
   # Calculate df and ncp
   p.body <- quote({
     stats::pt(stats::qt(alpha / side, n - 1, lower.tail = FALSE), n - 1,
-              sqrt(n) * delta / sigma, lower.tail = FALSE)
+              sqrt(n) * delta / sd, lower.tail = FALSE)
   })
 
   # The strict two-sided case uses the F distribution
   if (strict && side == 2)
     p.body <- quote({
-      ncp <- sqrt(n) * delta / sigma
+      ncp <- sqrt(n) * delta / sd
       stats::pf(stats::qf(alpha, 1, n - 1, lower.tail = FALSE),
                 1, n - 1, ncp^2, lower.tail = FALSE)
     })
@@ -56,8 +56,8 @@ pss.t.test.1samp <- function (n = NULL, delta = NULL, sigma = 1,
     power <- eval(p.body)
   else if (is.null(n))
     n <- uniroot(function(n) eval(p.body) - power, c(2, 1e+07))$root
-  else if (is.null(sigma))
-    sigma <- uniroot(function(sigma) eval(p.body) - power, delta * c(1e-07, 1e+07))$root
+  else if (is.null(sd))
+    sd <- uniroot(function(sd) eval(p.body) - power, delta * c(1e-07, 1e+07))$root
   else if (is.null(delta))
     delta <- uniroot(function(delta) eval(p.body) - power,  c(1e-07, 1e+07))$root
   else if (is.null(alpha))
@@ -68,7 +68,7 @@ pss.t.test.1samp <- function (n = NULL, delta = NULL, sigma = 1,
   METHOD <- "One-sample t test power calculation"
 
   # Print output as a power.htest object
-  structure(list(n = n, delta = delta, sigma = sigma, alpha = alpha,
+  structure(list(n = n, delta = delta, sd = sd, alpha = alpha,
                  power = power, one.or.two.sided = one.or.two.sided,
                  method = METHOD), class = "power.htest")
 }
--- R/pss.t.test.paired.R
+++ R/pss.t.test.paired.R
@@ -2,8 +2,8 @@
 #'
 #' @param n The sample size; the number of pairs.
 #' @param delta DeltaA (the true mean difference) - Delta0 (the mean difference under the null).
-#' @param sigma.pre The estimated pre standard deviation; defaults to 1.
-#' @param sigma.post The estimated post standard deviation; defaults to the same as sigma.pre.
+#' @param sd.pre The estimated pre standard deviation; defaults to 1.
+#' @param sd.post The estimated post standard deviation; defaults to 1.
 #' @param rho The estimated correlation between pre and post measurements on the same individual.
 #' @param alpha The significance level or type 1 error rate; defaults to 0.05.
 #' @param power The specified level of power.
@@ -15,22 +15,18 @@
 #'
 #' @examples
 #' # Example 3.15
-#' pss.t.test.paired(n = NULL, delta = 4, sigma.pre = 10, sigma.post = 10, rho = 0.4, power = 0.8, one.or.two.sided = "two")
+#' pss.t.test.paired(n = NULL, delta = 4, sd.pre = 10, sd.post = 10, rho = 0.4, power = 0.8, one.or.two.sided = "two")
 
 pss.t.test.paired <- function (n = NULL, delta = NULL,
-                               sigma.pre = 1, sigma.post = NULL, rho = NULL,
+                               sd.pre = 1, sd.post = 1, rho = NULL,
                                alpha = 0.05, power = NULL,
                                one.or.two.sided = c("two", "one"), strict = TRUE) {
 
-  # If sigma.post isnt' specified, default to the same as sigma.pre
-  if (!is.null(sigma.pre) && is.null(sigma.post))
-    sigma.post <- sigma.pre
-
   # Check if the arguments are specified correctly
-  if (sum(sapply(list(n, delta, sigma.pre, power, alpha), is.null)) != 1)
-    stop("exactly one of n, delta, sigma.pre, alpha, and power must be NULL")
-  if (is.null(rho))
-    stop("please specify a correlation rho")
+  if (sum(sapply(list(n, delta, power, alpha), is.null)) != 1)
+    stop("exactly one of n, delta, alpha, and power must be NULL")
+  if (is.null(sd.pre) | is.null(sd.post) | is.null(rho))
+    stop("please specify sd.pre, sd.post, and rho")
 
   # Assign number of sides
   one.or.two.sided <- match.arg(one.or.two.sided)
@@ -41,18 +37,18 @@ pss.t.test.paired <- function (n = NULL, delta = NULL,
     delta <- abs(delta)
 
   # Calculate the standard deviation of differences within pairs
-  sigma.d <- sqrt(sigma.pre^2 + sigma.post^2 - 2 * rho * sigma.pre * sigma.post)
+  sd.d <- sqrt(sd.pre^2 + sd.post^2 - 2 * rho * sd.pre * sd.post)
 
   # Calculate df and ncp
   p.body <- quote({
     stats::pt(stats::qt(alpha / side, n - 1, lower.tail = FALSE), n - 1,
-              sqrt(n) * delta / sigma.d, lower.tail = FALSE)
+              sqrt(n) * delta / sd.d, lower.tail = FALSE)
   })
 
   # The strict two-sided case uses the F distribution
   if (strict && side == 2)
     p.body <- quote({
-      ncp <- sqrt(n) * delta / sigma.d
+      ncp <- sqrt(n) * delta / sd.d
       stats::pf(stats::qf(alpha, 1, n - 1, lower.tail = FALSE),
                 1, n - 1, ncp^2, lower.tail = FALSE)
     })
@@ -62,11 +58,6 @@ pss.t.test.paired <- function (n = NULL, delta = NULL,
     power <- eval(p.body)
   else if (is.null(n))
     n <- uniroot(function(n) eval(p.body) - power, c(2, 1e+07))$root
-#  else if (is.null(sigma.pre)){
-#    sigma.pre <- uniroot(function(sigma.pre) eval(p.body) - power, delta * c(1e-07, 1e+07))$root
-#    sigma.ratio <- uniroot(function(sigma.ratio) eval(p.body) - power, c(1e-07, 1e+07))$root
-#    sigma.post <- sigma.pre * sigma.ratio
-#  }
   else if (is.null(delta))
     delta <- uniroot(function(delta) eval(p.body) - power,  c(1e-07, 1e+07))$root
   else if (is.null(alpha))
@@ -76,10 +67,10 @@ pss.t.test.paired <- function (n = NULL, delta = NULL,
   # Generate output text
   METHOD <- "Paired t test power calculation"
   NOTE <- "n is the number of pairs"
-  sigma <- c(sigma.pre, sigma.post)
+  sd <- c(sd.pre, sd.post)
 
   # Print output as a power.htest object
-  structure(list(n = n, delta = delta, sigma = sigma, rho = rho,
+  structure(list(n = n, delta = delta, sd = sd, rho = rho,
                  alpha = alpha, power = power, one.or.two.sided = one.or.two.sided,
-                 method = METHOD, note= NOTE), class = "power.htest")
+                 method = METHOD, note = NOTE), class = "power.htest")
 }
