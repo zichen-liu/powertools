64ba152f7674b387e79d4b3aa2c01760c8b0bfa2
Zichen Liu <77468946+zichen-liu@users.noreply.github.com>
2023-08-08 16:57:45 -0700

    updates to chisq documentation and variables

--- R/pss.chisq.gof.R
+++ R/pss.chisq.gof.R
@@ -1,22 +1,34 @@
+#' Power calculations for chi-square goodness-of-fit test
+#'
+#' @param p0vec The first vector of probabilities (under the null).
+#' @param p1vec The second vector of probabilities (under the alternative hypothesis).
+#' @param n The number of total observations.
+#' @param alpha The significance level or type 1 error rate; defaults to 0.05.
+#' @param power The specified level of power.
+#'
+#' @return A list of the arguments (including the computed one).
+#' @export
+#'
+#' @examples
 #' # Example 8.1
-#' pss.chisq.gof(p0 = c(0.5, 0.3, 0.2), p1 = c(0.7, 0.2, 0.1), n = 50)
+#' pss.chisq.gof(p0vec = c(0.5, 0.3, 0.2), p1vec = c(0.7, 0.2, 0.1), n = 50)
 
-pss.chisq.gof <- function (p0 = NULL, p1 = NULL,
+pss.chisq.gof <- function (p0vec = NULL, p1vec = NULL,
                            n = NULL, alpha = 0.05, power = NULL) {
 
   # Check if the arguments are specified correctly
   if (sum(sapply(list(n, alpha, power), is.null)) != 1)
     stop("exactly one of n, alpha, and power must be NULL")
-  if (is.null(p0) | is.null(p1))
+  if (is.null(p0vec) | is.null(p1vec))
     stop("please specify the two proportion vectors p0 and p1")
   if (!is.null(n) && any(n < 1))
     stop("number of observations must be at least 1")
-  if (length(p0) != length(p1))
+  if (length(p0vec) != length(p1vec))
     stop("the two proportion vectors must have the same lengths")
 
   # Calculate effect size and df
-  es <- sqrt(sum((p1 - p0)^2 / p0))
-  df <- length(p0) - 1
+  es <- sqrt(sum((p1vec - p0vec)^2 / p0vec))
+  df <- length(p0vec) - 1
 
   # Calculate test statistic
   p.body <- quote({
@@ -34,8 +46,8 @@ pss.chisq.gof <- function (p0 = NULL, p1 = NULL,
   else stop("internal error")
 
   # Generate output text
-  METHOD <- "Chi squared power calculation"
+  METHOD <- "Chi-square goodness-of-fit power calculation"
   NOTE <- "n is the number of observations"
-  structure(list(p1 = p1, p0 = p0, effect.size = es, n = n, alpha = alpha,
+  structure(list(p1vec = p1vec, p0vec = p0vec, effect.size = es, n = n, alpha = alpha,
                  power = power, method = METHOD, note = NOTE), class = "power.htest")
 }
--- R/pss.chisq.indep.R
+++ R/pss.chisq.indep.R
@@ -1,24 +1,35 @@
+#' Power calculations for chi-square test of independence
+#'
+#' @param pmatrix The two-way probability table under the alternative hypothesis.
+#' @param n The number of total observations.
+#' @param alpha The significance level or type 1 error rate; defaults to 0.05.
+#' @param power The specified level of power.
+#'
+#' @return A list of the arguments (including the computed one).
+#' @export
+#'
+#' @examples
 #' # Example 8.2
-#' pss.chisq.indep(p = matrix(c(0.050, 0.350, 0.100, 0.075, 0.250, 0.175), nrow = 2, byrow = TRUE), n = 230)
+#' pss.chisq.indep(pmatrix = matrix(c(0.050, 0.350, 0.100, 0.075, 0.250, 0.175), nrow = 2, byrow = TRUE), n = 230)
 #' # Example 8.3
-#' pss.chisq.indep(p = matrix(c(0.3, 0.2, 0.4, 0.1), nrow = 2, byrow = TRUE), n = 200)
+#' pss.chisq.indep(pmatrix = matrix(c(0.3, 0.2, 0.4, 0.1), nrow = 2, byrow = TRUE), n = 200)
 
-pss.chisq.indep <- function (p = NULL, n = NULL, alpha = 0.05, power = NULL) {
+pss.chisq.indep <- function (pmatrix = NULL, n = NULL, alpha = 0.05, power = NULL) {
 
   # Check if the arguments are specified correctly
   if (sum(sapply(list(n, alpha, power), is.null)) != 1)
     stop("exactly one of n, alpha, and power must be NULL")
-  if (is.null(p))
+  if (is.null(pmatrix))
     stop("please specify the proportion matrix p")
   if (!is.null(n) && any(n < 1))
     stop("number of observations must be at least 1")
 
   # Calculate effect size and df
-  pi <- apply(p, 1, sum)
-  pj <- apply(p, 2, sum)
+  pi <- apply(pmatrix, 1, sum)
+  pj <- apply(pmatrix, 2, sum)
   p0 <- pi %*% t(pj)
-  es <- sqrt(sum((p - p0)^2/p0))
-  df <- (nrow(p) - 1) * (ncol(p) - 1)
+  es <- sqrt(sum((pmatrix - p0)^2/p0))
+  df <- (nrow(pmatrix) - 1) * (ncol(pmatrix) - 1)
 
   # Calculate test statistic
   p.body <- quote({
@@ -36,10 +47,15 @@ pss.chisq.indep <- function (p = NULL, n = NULL, alpha = 0.05, power = NULL) {
   else stop("internal error")
 
   # Generate output text
-  METHOD <- "Chi squared power calculation"
+  METHOD <- "Chi-square test of independence power calculation"
   NOTE <- "n is the number of observations"
+  row.list <- c()
+  for (i in 1:nrow(pmatrix)) {
+    row.list <- c(row.list, paste(pmatrix[i,], collapse = ', '))
+  }
 
   # Print output as a power.htest object
-  structure(list(p = p, effect.size = es, n = n, alpha = alpha,
+  structure(list(pmatrix = paste(row.list, collapse = " | "),
+                 effect.size = es, n = n, alpha = alpha,
                  power = power, method = METHOD, note = NOTE), class = "power.htest")
 }
