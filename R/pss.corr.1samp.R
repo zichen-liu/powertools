12de75645a0713eb13157c63833079e202b12985
Zichen Liu <77468946+zichen-liu@users.noreply.github.com>
2023-09-13 16:18:50 -0700

    wrote slr function

--- R/pss.corr.1samp.R
+++ R/pss.corr.1samp.R
@@ -1,8 +1,8 @@
 #' Power calculations for one correlation coefficient
 #'
 #' @param n The sample size.
-#' @param r0 The correlation coefficient under the null hypothesis; defaults to 0.
-#' @param rA The correlation coefficient under the alternative hypothesis.
+#' @param rho0 The correlation coefficient under the null hypothesis; defaults to 0.
+#' @param rhoA The correlation coefficient under the alternative hypothesis.
 #' @param alpha The significance level or type 1 error rate; defaults to 0.05.
 #' @param power The specified level of power.
 #'
@@ -10,34 +10,34 @@
 #' @export
 #'
 #' @examples#' # Example 10.1
-#' pss.corr.1samp(n = 100, rA = 0.2)
-#' pss.corr.1samp(n = 100, r0 = 0.2, rA = 0.4)
+#' pss.corr.1samp(n = 100, rhoA = 0.2)
+#' pss.corr.1samp(n = 100, rho0 = 0.2, rhoA = 0.4)
 
-pss.corr.1samp <- function (n = NULL, r0 = 0, rA = NULL,
+pss.corr.1samp <- function (n = NULL, rho0 = 0, rhoA = NULL,
                             alpha = 0.05, power = NULL) {
 
   # Check if the arguments are specified correctly
   if (sum(sapply(list(n, power, alpha), is.null)) != 1)
     stop("exactly one of n, alpha, and power must be NULL")
-  if (is.null(rA))
-    stop("please specify rA")
+  if (is.null(rhoA))
+    stop("please specify rhoA")
   if (!is.null(n) && any(n < 4))
     stop("number of observations must be at least 4")
 
   # Calculate power
   p.body <- quote({
     za <- stats::qnorm(1 - alpha)
-    K1 <- (5 + rA^2) / (4 * (n - 1))
-    K2 <- (11 + 2 * rA^2 + 3 * rA^4) / (8 * (n - 1)^2)
+    K1 <- (5 + rhoA^2) / (4 * (n - 1))
+    K2 <- (11 + 2 * rhoA^2 + 3 * rhoA^4) / (8 * (n - 1)^2)
     K <- 1 + K1 + K2
 
-    om1 <- 0.5 * log((1 + rA) / (1 - rA))
-    om2 <- K * rA / (2 * (n - 1))
-    om3 <- 0.5 * log((1 + r0) / (1 - r0)) + r0 / (2 * (n - 1))
+    om1 <- 0.5 * log((1 + rhoA) / (1 - rhoA))
+    om2 <- K * rhoA / (2 * (n - 1))
+    om3 <- 0.5 * log((1 + rho0) / (1 - rho0)) + rho0 / (2 * (n - 1))
     omega <- sqrt(n - 3) * (om1 + om2 - om3)
 
-    nu1 <- (4 - rA^2) / (2 * (n - 1))
-    nu2 <- (22 - 6 * rA^2 - 3 * rA^4) / (6 * (n - 1)^2)
+    nu1 <- (4 - rhoA^2) / (2 * (n - 1))
+    nu2 <- (22 - 6 * rhoA^2 - 3 * rhoA^4) / (6 * (n - 1)^2)
     nu <- ((n - 3) / (n - 1)) * (1 + nu1 + nu2)
 
     stats::pnorm((omega - za) / sqrt(nu))
@@ -56,7 +56,7 @@ pss.corr.1samp <- function (n = NULL, r0 = 0, rA = NULL,
   METHOD <- "Single correlation coefficient power calculation"
 
   # Print output as a power.htest object
-  structure(list(n = n, r0 = r0, rA = rA, alpha = alpha, power = power,
+  structure(list(n = n, rho0 = rho0, rhoA = rhoA, alpha = alpha, power = power,
                  method = METHOD), class = "power.htest")
 
 
--- R/pss.corr.2samp.R
+++ R/pss.corr.2samp.R
@@ -1,8 +1,8 @@
 #' Power calculations for comparing two correlation coefficients
 #'
 #' @param n The sample size in each group.
-#' @param r1 The correlation coefficient in the first group.
-#' @param r2 The correlation coefficient in the second group.
+#' @param rho1 The correlation coefficient in the first group.
+#' @param rho2 The correlation coefficient in the second group.
 #' @param alpha The significance level or type 1 error rate; defaults to 0.05.
 #' @param power The specified level of power.
 #'
@@ -11,24 +11,24 @@
 #'
 #' @examples#' # Example 10.2
 #' library(psych)
-#' pss.corr.2samp(n = 300, r1 = 0.3, r2 = 0.1)
+#' pss.corr.2samp(n = 300, rho1 = 0.3, rho2 = 0.1)
 
-pss.corr.2samp <- function (n = NULL, r1 = NULL, r2 = NULL,
+pss.corr.2samp <- function (n = NULL, rho1 = NULL, rho2 = NULL,
                             alpha = 0.05, power = NULL) {
 
   # Check if the arguments are specified correctly
   if (sum(sapply(list(n, power, alpha), is.null)) != 1)
     stop("exactly one of n, alpha, and power must be NULL")
-  if (is.null(r1) | is.null(r2))
-    stop("please specify r1 and r2")
+  if (is.null(rho1) | is.null(rho2))
+    stop("please specify rho1 and rho2")
   if (!is.null(n) && any(n < 4))
     stop("number of observations must be at least 4")
 
   # Calculate power
   p.body <- quote({
     za <- stats::qnorm(1 - alpha)
-    f1 <- psych::fisherz(r1) + r1 / (2 * (n - 1))
-    f2 <- psych::fisherz(r2) + r2 / (2 * (n - 1))
+    f1 <- psych::fisherz(rho1) + rho1 / (2 * (n - 1))
+    f2 <- psych::fisherz(rho2) + rho2 / (2 * (n - 1))
     DeltaA <- abs(f1 - f2)
     lambda <- DeltaA / sqrt(1 / (n - 3) + 1 / (n - 3))
 
@@ -49,7 +49,7 @@ pss.corr.2samp <- function (n = NULL, r1 = NULL, r2 = NULL,
   NOTE <- "n is the number in each group"
 
   # Print output as a power.htest object
-  structure(list(n = n, r1 = r1, r2 = r2, alpha = alpha, power = power,
+  structure(list(n = n, rho1 = rho1, rho2 = rho2, alpha = alpha, power = power,
                  method = METHOD, note = NOTE), class = "power.htest")
 
 
