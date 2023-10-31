cf4ddd3e55c3a26beb6fa617bd591c3f257fa2b8
Zichen Liu <77468946+zichen-liu@users.noreply.github.com>
2023-10-30 23:40:05 -0700

    created pss.rr but can only solve for n

--- R/pss.rr.R
+++ R/pss.rr.R
@@ -1,10 +1,10 @@
-#' Power calculations for two sample proportion tests
+#' Power calculations for relative risk
 #'
 #' @param n The sample size for group 1.
 #' @param n.ratio The ratio n2/n1 between the sample sizes of two groups; defaults to 1 (equal group sizes).
 #' @param p1 The proportion in group 1.
 #' @param p2 The proportion in group 2.
-#' @param delta The margin of noninferiority or superiority; defaults to 0. See delta.sign for guidance on the sign of delta.
+#' @param RR0 The relative risk under the null; defaults to 1.
 #' @param alpha The significance level or type 1 error rate; defaults to 0.05.
 #' @param power The specified level of power.
 #' @param sides Either 1 or 2 (default) to specify a one- or two- sided hypothesis test.
@@ -13,11 +13,10 @@
 #' @export
 #'
 #' @examples
-#' pss.prop.2samp(n = NULL, p1 = 0.6, p2 = 0.8, alpha = 0.025, power = 0.9, sides = 1)
-#' pss.prop.2samp(n = NULL, p1 = 0.25, p2 = 0.25, delta = 0.1, alpha = 0.025, power = 0.8, sides = 1)
+#' pss.rr(n = NULL, n.ratio = 6, p1 = 0.1, p2 = 0.1 * 2, power = 0.8)
 
-pss.prop.2samp <- function (n = NULL, n.ratio = 1, p1 = NULL, p2 = NULL, delta = 0,
-                            alpha = 0.05, power = NULL, sides = 2) {
+pss.rr <- function (n = NULL, n.ratio = 1, p1 = NULL, p2 = NULL, RR0 = 1,
+                    alpha = 0.05, power = NULL, sides = 2) {
 
   # Check if the arguments are specified correctly
   if (sides != 1 & sides != 2)
@@ -29,11 +28,12 @@ pss.prop.2samp <- function (n = NULL, n.ratio = 1, p1 = NULL, p2 = NULL, delta =
 
   # Calculate test statistic
   p.body <- quote({
-    d <- abs(p1 - p2) - delta
+    RR <- p2 / p1
+    d <- abs(log(RR) - log(RR0))
     q1 <- 1 - p1
     q2 <- 1 - p2
-    ((stats::qnorm(alpha / sides) + stats::qnorm(1 - power))^2 *
-    (n.ratio * p1 * q1 + p2 * q2) / (n.ratio * d^2))
+    (stats::qnorm(1 - alpha / sides) + stats::qnorm(1 - power))^2 *
+    (1 + n.ratio) * (q1 / p1 + q2 / (n.ratio * p2)) / (d^2)
   })
 
   # Use uniroot function to calculate missing argument
@@ -49,12 +49,13 @@ pss.prop.2samp <- function (n = NULL, n.ratio = 1, p1 = NULL, p2 = NULL, delta =
 
   # Generate output text
   NOTE <- "n is the number in each group"
-  METHOD <- "Two sample comparison of proportions power calculation"
+  METHOD <- "Relative risk power calculation"
   n = c(n, n * n.ratio)
   p <- c(p1, p2)
+  RR <- c(RR, RR0)
 
   # Print output as a power.htest object
-  structure(list(n = n, `p1, p2` = p, delta = delta,
+  structure(list(n = n, `p1, p2` = p, `RR, RR0` = RR,
                  alpha = alpha, power = power, sides = sides, note = NOTE,
                  method = METHOD), class = "power.htest")
 }
