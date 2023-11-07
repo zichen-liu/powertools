65899bb4bd7deafb93a79ed096aae86b018406ba
Zichen Liu <77468946+zichen-liu@users.noreply.github.com>
2023-11-06 23:49:52 -0800

    fixed example running too long in manual

--- R/pss.anova2way.c.unbal.R
+++ R/pss.anova2way.c.unbal.R
@@ -15,7 +15,8 @@
 #' @examples
 #' nmatrix <- matrix(c(30, 30, 30, 30, 30, 30), nrow = 2, byrow = TRUE)
 #' mmatrix <- matrix(c(9.3, 8.9, 8.5, 8.7, 8.3, 7.3), nrow = 2, byrow = TRUE)
-#' pss.anova2way.c.unbal(nmatrix = nmatrix, mmatrix = mmatrix, cvec = c(1, 0, -1), factor = "b", sd = 2, alpha = 0.05)
+#' pss.anova2way.c.unbal(nmatrix = nmatrix, mmatrix = mmatrix, cvec = c(1, 0, -1),
+#' factor = "b", sd = 2, alpha = 0.05)
 
 pss.anova2way.c.unbal <- function (nmatrix = nmatrix, mmatrix = NULL, cvec = NULL,
                             factor = c("a", "b"), sd = NULL, Rsq = 0, ncov = 0,
