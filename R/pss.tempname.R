43c00c362933563d66f552b324a2f2f6f835570c
Zichen Liu <77468946+zichen-liu@users.noreply.github.com>
2024-05-09 12:13:23 -0700

    created function for correlation btwn baseline & followup means

---
+++ R/pss.tempname.R
@@ -0,0 +1,25 @@
+#' Correlation between a cluster mean at baseline and follow up
+#'
+#' @param m The number of measurements in each cluster at baseline and follow up.
+#' @param icc The intraclass correlation coefficient.
+#' @param cac The cluster autocorrelation.
+#' @param sac The subject autocorrelation.
+#' @return The computed correlation.
+#' @export
+#'
+#' @examples
+#' pss.tempname(m = 30, icc = 0.05, cac = 0.4, sac = 0.5)
+
+
+pss.tempname <- function (m, icc, cac, sac) {
+  pss.check(m, "req"); pss.check(m, "pos")
+  pss.check(icc, "req"); pss.check(icc, "uniti")
+  pss.check(cac, "req"); pss.check(cac, "uniti")
+  pss.check(sac, "req"); pss.check(sac, "uniti")
+
+  denom <- 1 + (m - 1) * icc
+  num <- m * icc * cac + (1 - icc) * sac
+  r <- num / denom
+
+  return(r)
+}
